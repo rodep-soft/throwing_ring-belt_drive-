c485045d2bea66c872ffe32f64010c59a48efceb
CyaNabe <abe.mitsunobu2188@mail.kyutech.jp>
2026-04-16 10:56:55 +0900

    write

--- ros2_ws/src/launcher/src/launcher_node.cpp
+++ ros2_ws/src/launcher/src/launcher_node.cpp
@@ -18,8 +18,18 @@ class LauncherNode : public rclcpp::Node {
 		}
 
 	private:
-		float target_height = 0.70;
-		float target_distance;
+		float target_height_ = 0.70;
+		float m = 0.43;  // mass
+		float g_ = 9.8;  // gravitational acceleraition
+		float drag_coefficient ;
+		float density;
+		float area;
+		float k = drag_coefficient * density * area / 2;
+		float gamma_ = k / m;
+		float angle = M_PI / 4;
+		float tan_theta_ = std::tan(angle);
+		float cos_theta_ = std::cos(angle);
+		float target_distance_ = 0;
 		bool is_shooting = false;
 
 		rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscriber_;
@@ -37,15 +47,26 @@ class LauncherNode : public rclcpp::Node {
 			RCLCPP_INFO(this->get_logger(), "distance middle %f ", msg->ranges[540]);
 			RCLCPP_INFO(this->get_logger(), "num of data %zu", msg->ranges.size());
 
-			//new_target_distance = calculate_distance_average(msg);
-			int n = 10;
-			for(int i=0; i<n;i++){
-				float distance = msg->ranges[msg->ranges.size()-n)/2+i+1];
-				if(distance > 0 && distance < 40){
-						}
-			}
+			target_distance_ = calculate_distance_average(msg);
 			//queueにいれる();
 
+		}
+
+		float calculate_distance_average(const sensor_msgs::msg::LaserScan::SharedPtr msg){
+			int n = 10;
+			float sum_distance = 0;
+			int count = 0;
+			for(int i=0; i<n;i++){
+				float distance = msg->ranges[(msg->ranges.size()-n)/2+i+1];
+				if(distance > 0 && distance < 40){
+					sum_distance += distance;
+					count ++;
+				}
+			}
+			
+			if (count == 0) return 0.0;
+
+			return sum_distance / count;
 		}
 
 		void handle_service(const std::shared_ptr<std_srvs::srv::Empty::Request> request,
@@ -55,13 +76,42 @@ class LauncherNode : public rclcpp::Node {
 			(void)response;
 
 			is_shooting = true;
-
-		//----------calculate goal position()
+			float velocity = solve_velocity(target_distance_, target_height_);
+			
+			//shoot(velocity);
 		}
-		float calculate_goal_position(float target_distance, float target_height){
-		//-------------------------
-			target_distance;
-			target_height;
+		float solve_velocity(float dist, float height){
+			float v = 8.0;
+			const float epsilon = 1e-3;
+			const int max_iter = 100;
+			int iter = 0;
+			
+			float y_error = calc_y_error(v, dist, height);  
+			float dy_dv = calc_dy_dv(v, dist);
+			while(std::abs(y_error) > epsilon && iter < max_iter) {
+				
+
+				if (dy_dv < 1e-6) break;
+
+				v = v - y_error / dy_dv;
+				iter ++;
+
+				y_error = calc_y_error(v, dist, height);  
+				dy_dv = calc_dy_dv(v, dist);
+			}
+			
+
+			return v;
+		}
+
+		float calc_y_error(float v0, float x, float target_y){
+			float y = (tan_theta_ + g_ /gamma_ /v0 /cos_theta_)*x +g_ /gamma_/gamma_ *std::log(1 -gamma_ *x /v0 /cos_theta_);
+			return y - target_y;
+		}
+
+		float calc_dy_dv(float v0, float x) {
+			float dy_dv = g_ *x*x / (v0*v0*cos_theta_ *(v0 *cos_theta_ -gamma_ *x));
+			return dy_dv;
 		}
 };
 
